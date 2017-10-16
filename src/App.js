dffe2c27220dd79610c7ccce6a5bafa7896357dc
Cristian Muscalu <c_muscalu@mgcorp.co>
2017-10-16 15:47:58 +0300

    added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Adi app</h1>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Adi app</h1>
+          </header>
+          <RaisedButton label="Click Me" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }

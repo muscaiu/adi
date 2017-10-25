6c81ce983d5ff2c0dad863767406b4495662d2d0
Cristian Muscalu <muscaiu@yahoo.com>
2017-10-25 12:53:29 +0300

    loading  comments from the DB to the page

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,25 +1,50 @@
 //CommentBox.js
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Header from './Header'
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
+// import DATA from '../data';
 import style from './style';
 
 
 class CommentBox extends Component {
     constructor(props) {
         super(props);
-        this.state = { data: [] };
+        this.state = {
+            data: [],
+            url: 'http://localhost:3001/api/comments',
+            pollInterval: 6000
+        }
+        this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.state.pollInterval);
+    }
+    loadCommentsFromServer() {
+        axios.get(this.state.url)
+            .then(res => {
+                this.setState({ data: res.data })
+            })
+        // fetch(this.state.url)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         this.setState({ data: data })
+        //     })
+    }
+    handleCommentSubmit(comment) {
+        //add POST request
     }
     render() {
         return (
             <div style={style.commentBox}>
                 <Header />
                 <h2>Comments:</h2>
-                <CommentList data={DATA} />
-                <CommentForm />
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         )
     }
